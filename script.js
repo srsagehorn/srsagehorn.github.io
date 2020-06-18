acf89e6f05006939eed5bb9e9b674b9265704b80
Shannon Sagehorn <srsagehorn@gmail.com>
2020-06-18 00:55:18 -0500

    sticky nav

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function typeWriter() {
 setTimeout(function () {
   typeWriter();
 }, 500);
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+  scrollFunction();
+};
+// console.log($(window).scrollTop());
+// function scrollFunction() {
+//   if ($("body").scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     // $(".logoName").attr("style", "display : none");
+//     $("h1").addClass("navHidden");
+//     $(".navImg").attr("style", "display : none");
+//   }
+//   if ($(window).scrollTop() == 0) {
+//     $("h1").removeClass("navHidden");
+//     $(".navImg").removeAttr("style");
+//   }
+// }
+
+// when scroll add attr display: none to .logoName
